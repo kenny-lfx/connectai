bba617daa34820323641dc77a9c8ad538b0952a8
KennyL <58063352+kenny-lfx@users.noreply.github.com>
2024-10-01 09:57:40 -0700

    Add files via upload

---
+++ connectAI-backend/constants/constants.go
@@ -0,0 +1,11 @@
+package constants
+
+
+const (
+    USER_INDEX = "user"
+    POST_INDEX = "post"
+    ES_URL = "http://10.128.0.2:9200"
+    ES_USERNAME = "esusername"
+    ES_PASSWORD = "espassword"
+    GCS_BUCKET = "socialai-bucket1"
+)
